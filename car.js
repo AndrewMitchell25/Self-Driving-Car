af4ae77c04344d3ccc4f3b0d245d740e95c5e159
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-05-12 17:43:55 -0700

    Controls fully working

--- car.js
+++ car.js
@@ -5,12 +5,65 @@ class Car{
         this.width = width;
         this.height = height;
 
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
+
         this.controls = new Controls();
     }
 
+    update(){
+        //use acceleration 
+        if(this.controls.forward){
+            this.speed += this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed -= this.acceleration;
+        }
+        //cap the speed
+        if(this.speed > this.maxSpeed){
+            this.speed = this.maxSpeed;
+        }
+        if(this.speed < -this.maxSpeed/2){
+            this.speed = -this.maxSpeed/2;
+        }
+        //add friction
+        if(this.speed > 0){
+            this.speed -= this.friction;
+        }
+        if(this.speed < 0){
+            this.speed += this.friction;
+        }
+        //set speed to 0 if very small
+        if(Math.abs(this.speed) < this.friction){
+            this.speed = 0;
+        }
+
+        //left and right controls
+        if(this.controls.left){
+            this.angle += 0.03;
+        }
+        if(this.controls.right){
+            this.angle -= 0.03;
+        }
+        //use unit circle to implement movement
+        this.x -= Math.sin(this.angle)*this.speed;
+        this.y -= Math.cos(this.angle)*this.speed;
+    }
+
     draw(ctx){
+        ctx.save();
+        //move the context to the x and y positions
+        ctx.translate(this.x, this.y);
+        //rotate the context
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
-        ctx.rect(this.x - this.width/2, this.y-this.height/2, this.width, this.height);
+        ctx.rect(-this.width/2, -this.height/2, this.width, this.height);
         ctx.fill();
+
+        ctx.restore();
     }
 }
