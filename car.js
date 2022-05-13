0bb9db3cad4b87a9c588bed1fef940583903af5a
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-05-12 22:09:55 -0700

    Sensors and Intersections working

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car{
         this.friction = 0.05;
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-    update(){
+    update(roadBorders){
         this.#move();
+        this.sensor.update(roadBorders);
     }
 
     #move(){
@@ -75,5 +77,8 @@ class Car{
         ctx.fill();
 
         ctx.restore();
+
+        //draw the sensor
+        this.sensor.draw(ctx);
     }
 }
